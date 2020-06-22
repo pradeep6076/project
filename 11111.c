3c9470620789aac6b53b735f1eca4e5df9bc691b
pradeep6076 <63346256+pradeep6076@users.noreply.github.com>
2020-06-22 19:09:39 +0530

    new file adding

---
+++ 11111.c
@@ -0,0 +1,21 @@
+#include<stdio.h>    
+void main ()    
+{    
+    int i, j,temp,largest,sec_largest;     
+    int a[10] = { 10, 9, 7, 101, 23, 44, 12, 78, 34, 23};    
+	 largest=arr[0];
+	 sec_largest=arr[1]; 
+    for(i = 0; i<10; i++)    
+    {    
+        if(a[i]>largest) 
+		{
+			sec_largest=largest;
+			largest=a[i];
+			} 
+			elseif(a[i]>sec_largest & a[i]!=largest)
+			{
+				sec_largest=a[i];
+			   }   
+    }     
+     printf("largest = %d, second largest = %d",largest,sec_largest);   
+}
